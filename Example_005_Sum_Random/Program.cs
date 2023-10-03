6671a7a8205e996ec12e41f585143af63216e828
Alexandr <schurik1983@bk.ru>
2023-10-03 22:59:56 +0300

    Example_005_Sum_Random

---
+++ Example_005_Sum_Random/Program.cs
@@ -0,0 +1,7 @@
+﻿int numberA = new Random().Next(2, 10);
+int numberB = new Random().Next(10, 34);
+Console.Write(numberA);
+Console.Write('+');
+Console.Write(numberB);
+Console.Write('=');
+Console.Write(numberA + numberB);
